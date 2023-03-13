f500387eb8d13ccac6c5bad67192d215e849d8ea
max <max.tyushkevich@gmail.com>
2023-03-13 11:14:41 +0300

    Initial commit

---
+++ src/test/java/com/example/labjava/LabJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.labjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
